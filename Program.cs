57278c1ef0601b13fd873325cc2fd33031772192
LuisGaleaas <luisgaleas2005@gmail.com>
2023-11-06 16:48:54 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ejercicioParcialito.models;
+
+namespace ejercicioParcialito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            producto producto = new producto();
+            Dictionary<string, List<producto>> ListaProductos = new Dictionary<string, List<producto>>();
+
+            while (true)
+            {
+                Console.WriteLine("BIENVENIDOS A LA TIENDA DE TECNOLOGIA DE LA UES");
+                Console.WriteLine("MENU DE OPCIONES");
+                Console.WriteLine("1- Menu de productos de la tienda");
+                Console.WriteLine("2- Agregar una compra");
+                Console.WriteLine("3- Lista de compra");
+                Console.WriteLine("4- Buscar compra");
+                Console.WriteLine("5- Salir del programa");
+
+                ;
+                string respuesta = "";
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+
+                    case "1":
+                        Console.WriteLine("PlayStation 5 || precio : 800");
+                        Console.WriteLine("Iphone 15 || precio : 1,500");
+                        Console.WriteLine("Mouse HyperX || precio : 49.50");
+                        Console.WriteLine("Teclado Razer || precio : 50.00");
+                        break;
+
+                    case "2":
+                        producto.pedirDatos();
+
+                        break;
+                    case "3":
+                        producto.mostrarInformacion();
+                        break;
+                    case "4":
+                        producto.buscarTicket();
+
+
+
+                        break;
+                    case "5":
+                        do
+                        {
+                            Console.WriteLine("Desea salir del programa? \n Apriete S para SI , \n cualquier otra letra pra NO");
+                            respuesta = Console.ReadLine();
+
+                        } while (respuesta.Equals("S", StringComparison.OrdinalIgnoreCase));
+
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ models/producto.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioParcialito.models
+{
+    class producto
+    {
+        public int numTicket { get; set; }
+        public string nombreCliente { get; set; }
+        public string nombreProducto { get; set; }
+        public int cantidad { get; set; }
+        public double precio { get; set; }
+        public double totalxProducto { get; set; }
+        public double precioCompra { get; set; }
+
+
+        private Dictionary<string, double> productos = new Dictionary<string, double>
+    {
+        { "PlayStation 5", 800.0 },
+        { "Iphone 15", 1500.0 },
+        { "Mouse HyperX", 49.5 },
+        { "Teclado Razer", 50.00}
+    };
+
+        public void mostrarDatosProductos()
+        {
+            foreach (var producto in productos)
+            {
+                Console.WriteLine($"{producto.Key} || precio : ${producto.Value}");
+            }
+        }
+
+        public void pedirDatos()
+        {
+            Console.WriteLine("Ingrese su numero de ticket");
+            numTicket = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su nombre");
+            nombreCliente = Console.ReadLine();
+            Console.WriteLine("Ingrese la cantidad de productos que desea comprar");
+            cantidad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el nombre del producto que desea ordenar");
+            nombreProducto = Console.ReadLine();
+
+
+            if (productos.TryGetValue(nombreProducto, out double precioProducto))
+            {
+                precio = precioProducto;
+            }
+            else
+            {
+                Console.WriteLine("Producto no encontrado en la lista.");
+            }
+        }
+
+        public void mostrarInformacion()
+        {
+            Console.WriteLine($"Numeo de ticket -> {this.numTicket}");
+            Console.WriteLine($"Nombre del cliente -> {this.nombreCliente}");
+            Console.WriteLine($"Cantidad de producto a comprar -> {this.cantidad}");
+            Console.WriteLine($"Nombre del producto comprado -> {this.nombreProducto}");
+            Console.WriteLine($"Precio del producto -> ${this.precio}");
+            Console.WriteLine($"Precio total de la compra -> ${this.precio * this.cantidad}");
+        }
+        public void buscarTicket()
+        {
+            int buscar = 0;
+            Console.WriteLine("Ingrese el ticket a buscar");
+            buscar = int.Parse(Console.ReadLine());
+            if (numTicket == buscar)
+            {
+                mostrarInformacion();
+            }
+        }
+
+    }
+}
